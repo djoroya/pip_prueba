782d6796b2aeb87f701d736a3c5400f23d254ad9
Jesus Oroya (Ubuntu Casa) <41575404+djoroya@users.noreply.github.com>
2023-12-17 17:35:20 +0100

    deploy

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-from setup import setup, find_packages
+from setuptools import setup, find_packages
 
-setup( name='my_package',
-      description='My package',
-        version='0.1',
-        packages=find_packages(),
-        install_requires=['numpy', 'scipy', 'matplotlib'],
-        author='Deyviss Jesús Oroya-Villalta',
-        license='MIT',
-        url='https://github.com/djoroya/pip_prueba',
-        classifiers=[
-            'Development Status :: 1 - Planning',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3.6',
-            'Topic :: Scientific/Engineering :: Atmospheric Science',
-            'Topic :: Scientific/Engineering :: Physics',
-            'Topic :: Scientific/Engineering :: Visualization',
-            'Topic :: Software Development :: Libraries :: Python Modules',
-        ],
-        keywords='my_package',
-        )
+if __name__ == '__main__':
+  setup( name='my_package',
+        description='My package',
+          version='0.1',
+          packages=find_packages(),
+          install_requires=['numpy', 'scipy', 'matplotlib'],
+          author='Deyviss Jesús Oroya-Villalta',
+          license='MIT',
+          url='https://github.com/djoroya/pip_prueba',
+          classifiers=[
+              'Development Status :: 1 - Planning',
+              'Intended Audience :: Science/Research',
+              'License :: OSI Approved :: MIT License',
+              'Programming Language :: Python :: 3.6',
+              'Topic :: Scientific/Engineering :: Atmospheric Science',
+              'Topic :: Scientific/Engineering :: Physics',
+              'Topic :: Scientific/Engineering :: Visualization',
+              'Topic :: Software Development :: Libraries :: Python Modules',
+          ],
+          keywords='my_package',
+          )
